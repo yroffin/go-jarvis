97ea6227da0af781bee1a92b8f9e5b0fc18e12e2
Yannick Roffin <yroffin@gmail.com>
2018-04-18 22:58:33 +0200

    Corrections

--- auto/auto.go
+++ auto/auto.go
@@ -33,9 +33,7 @@ import (
 
 func init() {
 	// Log as JSON instead of the default ASCII formatter.
-	log.SetFormatter(&log.TextFormatter{
-		ForceColors: true,
-	})
+	log.SetFormatter(&log.JSONFormatter{})
 
 	// Output to stdout instead of the default stderr
 	// Can be any io.Writer, see below for File example

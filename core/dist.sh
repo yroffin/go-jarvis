368ab6b367f8b5f156c3c6d915b85b21f99867a4
Yannick Roffin <yroffin@gmail.com>
2018-04-21 19:28:11 +0200

    Correction

--- core/dist.sh
+++ core/dist.sh
@@ -1,5 +1,5 @@
 rm -rf dist/*
 mkdir -p dist/jarvis-ui
-cp ../frontend/dist.tar dist/dist.tar
+cp ../ui/dist.tar dist/dist.tar
 cp -rf resources/static/swagger-ui dist
 cd dist/jarvis-ui && tar xvf ../dist.tar && rm -f ../dist.tar

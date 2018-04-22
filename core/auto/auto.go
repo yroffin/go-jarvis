42ee9908ae77a3a4330ff11271feb9230665fbf6
Yannick Roffin <yroffin@gmail.com>
2018-04-22 09:05:25 +0200

    Exp

--- core/auto/auto.go
+++ core/auto/auto.go
@@ -34,7 +34,7 @@ import (
 
 func init() {
 	// Log as JSON instead of the default ASCII formatter.
-	log.SetFormatter(&log.JSONFormatter{})
+	log.SetFormatter(&log.TextFormatter{})
 
 	// Output to stdout instead of the default stderr
 	// Can be any io.Writer, see below for File example
